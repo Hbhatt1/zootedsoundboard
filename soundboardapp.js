99ad57965a1e008fa29a7125b4c0ec4e8024ee4e
Hbhatt1 <8009159+Hbhatt1@users.noreply.github.com>
2023-03-13 13:51:47 +0000

    Add files via upload

---
+++ soundboardapp.js
@@ -0,0 +1,40 @@
+const sounds = [
+    "YADCKYS", 
+    "CCC", 
+    "BYNTGAGB", 
+    "Y2AFZ", 
+    "ENEMIES", 
+    "ARGHHHHH", 
+    "ASDFGHJKL",
+    "WHATEVAMAN",
+    "WGAF",
+    "YSASU",
+    "DMWSWFYFR",
+    "BUBUBUBUBU",
+    "LWIJSYSC",
+    "AREYOUZOOTED",
+    "AREYOUAGOBLIN"
+];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopAudio();
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopAudio() {
+  sounds.forEach((sound) => {
+    const clip = document.getElementById(sound);
+
+    clip.pause();
+    clip.currentTime = 0;
+  });
+}
